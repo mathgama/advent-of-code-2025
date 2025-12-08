e768a43c370b7e26cc9d88ed8888f46c16a545ee
Matheus Gama <matheus_gama@live.com>
2025-12-08 19:12:01 -0300

    day01 second star

--- src/day01/day01.js
+++ src/day01/day01.js
@@ -24,7 +24,43 @@ const firstStar = () => {
     if (dialPointingAt == 0) password += 1
   })
 
-  console.log(password)
+  console.log('First star: ' + password)
+}
+
+const secondStar = () => {
+  let password = 0
+  let dialPointingAt = 50
+
+  input.forEach((rotation) => {
+    const direction = rotation.slice(0, 1)
+    const numberOfClicks = +rotation.slice(1)
+
+    switch (direction) {
+      case 'L':
+        if (dialPointingAt == '0') password--
+
+        dialPointingAt -= numberOfClicks
+        break
+      case 'R':
+        dialPointingAt += numberOfClicks
+        break
+    }
+
+    while (dialPointingAt < 0) {
+      dialPointingAt += 100
+      password++
+    }
+
+    if (dialPointingAt == '0') password++
+
+    while (dialPointingAt >= 100) {
+      dialPointingAt -= 100
+      password++
+    }
+  })
+
+  console.log('Second star: ' + password)
 }
 
 firstStar()
+secondStar()
